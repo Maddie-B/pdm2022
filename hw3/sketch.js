55c414ba82ee8d79a82adddbc2d146dcfcdadbdd
Maddie-B <maddieb428@gmail.com>
2022-02-09 21:53:15 -0600

    updated homework

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -1,93 +1,165 @@
+let spriteSheet;
+let spriteSheet2;
+let spriteSheet3;
+let character = [];
+let count = 10;
+
+function preload()
 {
-  let character;
-  let character2;
+ spriteSheet = loadImage("SpelunkyGuy.png");
+ spriteSheet2 = loadImage("Green.png");
+ spriteSheet3 = loadImage("Ninja.png");
+} 
 
-  function preload()
- {
-   spriteSheet = loadImage("SpelunkyGuy.png");
- } 
+function setup() 
+{
+ createCanvas(600, 600);
+ imageMode(CENTER);
 
-  function setup() 
+ for (i = 0; i < count; i++)
  {
-   createCanvas(600, 600);
-   imagemode(CENTER);
-   character = new Character(spriteSheet, 0, 300);
-   character2 = new Character(spriteSheet, 300, 300);
+   character[i] = new Character(random([spriteSheet, spriteSheet2, spriteSheet3]), random(100, 500), random(100, 500), random(1, 5), random([-1, 1]));
  }
+}
 
- function keyPressed()
- {
-   if (keyCode == RIGHT_ARROW)
-     {
-       character.go(1);
-       character2.go(1);
-     }
-   else if (keyCode == LEFT_ARROW)
-     {
-       character.go(-1);
-       character2.go(-1);
-     }
- }
+function keyPressed()
+{
+  if (keyCode == RIGHT_ARROW)
+  {
+    for (i = 0; i < count; i++)
+    {
+      character[i].go(1);
+    }    
+  }
+  else if (keyCode == RIGHT_ARROW)
+  {
+    for (i = 0; i < count; i++)
+    {
+      character[i].go(-1);
+    }
+  }    
+}
 
- function keyReleased()
- {
-   character.stop();
-   character2.stop();
- }
+function keyReleased()
+{
+  for (i = 0; i < count; i++)
+  {
+    character[i].stop();
+  }    
+}
 
- function draw() 
- {
-   background(225, 225, 225);
-   character.draw();
-   character2.draw();
- }
+function Released()
+{for (i = 0; i < count; i++)
+  {
+   character[i].drop();
+  }
+}
 
- let temp = Character()
+function mouseDragged()
+{
+  for (i = 0; i < count; i++)
+  {
+   character[i].drag();
+  }
+}
+
+function mousePressed()
+{for (i = 0; i < count; i++)
+  {
+   character[i].grab();
+  }
+}
+
+function draw() 
+{
+  background(225, 225, 225);
+  for (i = 0; i < count; i++)
+  {
+    character[i].draw();
+  }
+}
      
- class Character
- {
-   constructor(spriteSheet, x, y)
-   {
-     this.spriteSheet = spriteSheet;
-     this.sx = 0;
-     this.x = x; 
-     this.y= y;
-     this.move = 0;
+class Character
+{
+  constructor(spriteSheet, x, y, speed, move)
+  {
+    this.spriteSheet = spriteSheet;
+    this.sx = 0;
+    this.x = x; 
+    this.y= y;
+    this.move = 0;
+    this.facing = 1;
+    this.speed = speed;
+    this.move = move;
+  }
+
+  draw()
+  {
+    push();
+    translate(this.x, this.y);
+    scale (this.facing, 1);
+
+    if (this.move == 0)
+    {
+      image(this.spritesheet, 0, 0, 100, 100, 0, 0, 80, 80);
+    }
+    else
+    { 
+      image(this.spritesheet, 0, 0, 100, 100, 80 * (this.sx + 1), 0, 80, 80);
+    }
+    if (frameCount % 5 == 0)
+    {
+      this.sx = (this.sx + 1) % 8;
+    }
+    this.x += 2 * this.move;
+
+    if (this.x < 30)
+    {
+     this.move = 1;
      this.facing = 1;
-   }
- }
+    }
+    else if (this.x > width - 30)
+    {
+      this.move = -1;
+      this.facing = -1;
+    }
 
- draw()
- {
-   push();
-   translate(this.x, this.y);
-   scale (this.facing, 1);
-   if (this.move == 0)
-     {
-       image(this.spritesheet, 0, 0, 200, 200, 0, 0, 80, 80);
-     }
-   else
-     {
-       image(this.spritesheet, 0, 0, 200, 200, 80 * (this.sx + 1), 0, 80, 80);
-     }
-   if (frameCount % 5 == 0)
-     {
-       this.sx = (this.sx + 1) % 8;
-     }
-   this.x += 2 * this.move;
-   pop();
- }
+    pop();
+  }
 
- go(direction)
- {
-   this.move = direction;
-   this.facing = direction;
-   this.sx = 3;
- }
+  go(direction)
+  {
+    this.move = direction;
+    this.facing = direction;
+    this.sx = 3;
+  }
 
- stop()
- {
-   this.move= 0;
- }
+  stop()
+  {
+    this.move= 0;
+  }
+  
+  grab()
+  {if (mouseX > this.x - 40 && mouseX < this.x + 40 && mouseY > this.y -40 && mouseY < this.y + 40)
+    {
+     this.stop();
+     this.grabbed = true;
+    }
+  }
 
+  drag()
+  {if (grabbed)
+    {
+     this.x = mouseX;
+     this.y = mouseY;
+    }
+  }
+
+  drop()
+  {if (mouseX > this.x - 40 && mouseX < this.x + 40 && mouseY > this.y -40 && mouseY < this.y + 40)
+    {
+     this.stop();
+     this.dropped = true;
+    }
+  }
 }
